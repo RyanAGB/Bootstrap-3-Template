389eb6c83b6d5d0393479eb78d1e45b4f65a87a2
RyanAGB <raborja01@gmail.com>
2014-02-12 23:51:33 +0800

    Added new styling and javascript

--- js/custom.js
+++ js/custom.js
@@ -8,9 +8,41 @@ $(document).ready(function(){
 		email: 'email@gmail.com', //optional, can have multiple values[array]
 		address: '4 First Light Ct, Coomera waters 4209' //optional, single value
 	});
+
+	//Slider
+	$('.camera_wrap').shuffleCamera();
+    jQuery('.camera_wrap').camera({
+        playPause: false,
+        pagination: false,
+        loader: 'none',
+        navigation: false,
+        portrait: true,
+        height: '26.07692307692308%'
+    });
+
+    //Validation
+    $('#custom_form').formValidation({ 
+		validateText: ["name"],
+		validateEmail: ["email"],
+		validateSpam: true
+	}); 
      
-}); 
+}); // end declaration
 
 
-$('#view_section_1').insertAfter('#slider');
- $('#headerdiv').insertBefore.('#content');
+//$('#view_section_1').insertAfter('#slider');
+//$('#headerdiv').insertBefore.('#content');
+
+ (function($){
+    $.fn.shuffleCamera = function() {
+        return this.each(function(){
+            var items = $(this).children().clone(true);
+            return (items.length) ? $(this).html($.shuffleCamera(items)) : this;
+        });
+    }
+    
+    $.shuffleCamera = function(arr) {
+        for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+        return arr;
+    }
+})(jQuery);
