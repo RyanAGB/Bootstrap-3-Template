657a67e017bdd1a8452374d895df7c1830c16271
RyanAGB <raborja01@gmail.com>
2014-02-12 23:33:40 +0800

    Added the raw file

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+//Javascripts
+$(document).ready(function(){ 
+	$.favicon('favicon.ico');
+	
+	menu.contactDetails({
+		phone: ['0471948576', '0471948576'], //optional, can have multiple values[array]
+		mobile: '0452857464', //optional, can have multiple values[array]
+		email: 'email@gmail.com', //optional, can have multiple values[array]
+		address: '4 First Light Ct, Coomera waters 4209' //optional, single value
+	});
+     
+}); 
+
+
+$('#view_section_1').insertAfter('#slider');
+ $('#headerdiv').insertBefore.('#content');
